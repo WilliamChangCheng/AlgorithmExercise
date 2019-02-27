0857bc42dccf33f36acbb9e6d6293faf0bd07104
william <changcheng0609@gmail.com>
2019-02-27 16:24:08 +0800

    提交

---
+++ idea exicise/src/linkList/RemoveDuplicatesFromSortedList2.java
@@ -0,0 +1,116 @@
+package linkList;
+
+/**
+ * 题目
+ * <p>
+ * 给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字。
+ * <p>
+ * 示例 1:
+ * 输入: 1->2->3->3->4->4->5
+ * 输出: 1->2->5
+ * <p>
+ * 示例 2:
+ * 输入: 1->1->1->2->3
+ * 输出: 2->3
+ */
+
+public class RemoveDuplicatesFromSortedList2 {
+
+    /**
+     * 递归法（很快）
+     * 很难理解
+     *
+     * @param head
+     * @return
+     */
+    static ListNode r;
+
+    public static void main(String[] args) {
+        ListNode line = Tool.stringToListNode("[1,1,2,3,3]");
+        ListNode res = deleteDuplicates2(line);
+    }
+
+    /**
+     * 自己写的，太罗嗦
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode deleteDuplicates(ListNode head) {
+
+        if (head == null) return null;
+        int num = head.val - 1;
+        ListNode res = new ListNode(0);
+        res.next = head;
+        ListNode temp = res;
+        ListNode preTemp = res;
+        while (head != null) {
+            //若果等于上一次记的数
+            if (head.val == num) {
+                //下一个数前移到temp位置，检查temp位置数据是否改变，未改变就接着前移
+                while (temp.val == num) {
+                    if (temp.next != null) {
+                        temp.val = temp.next.val;
+                        temp.next = temp.next.next;
+                    } else {
+                        preTemp.next = null;
+                        return res.next;
+                    }
+
+                }
+                num = temp.val;
+                head = temp.next;
+            } else {
+                //temp前移,前移之前先给pretemp，防止尾部数据是重复数据
+                preTemp = temp;
+                temp = head;
+                //从新记录
+                num = head.val;
+                head = head.next;
+            }
+
+        }
+        return res.next;
+    }
+
+    /**
+     * 不罗嗦的循环
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode deleteDuplicates1(ListNode head) {
+        if (head == null) return null;
+        ListNode temp = new ListNode(0);
+        temp.next = head;
+        head = temp;
+        ListNode left, right;
+        while (temp.next != null) {
+            left = temp.next;
+            right = left;
+            while (right.next != null && right.next.val == left.val) {
+                right = right.next;
+            }
+            if (left == right) temp = temp.next;
+            else temp.next = right.next;
+        }
+        return head.next;
+    }
+
+    public static ListNode deleteDuplicates2(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        if (head.next != null && head.val == head.next.val) {
+            while (head.next != null && head.val == head.next.val) {
+                head = head.next;
+            }
+            return r = deleteDuplicates2(head.next);
+        } else {
+            head.next = deleteDuplicates2(head.next);
+        }
+
+        return head;
+    }
+
+}
---
+++ idea exicise/src/linkList/ReverseLinkedList2.java
@@ -0,0 +1,76 @@
+package linkList;
+
+/**
+ * 反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
+ * 说明:
+ * 1 ≤ m ≤ n ≤ 链表长度。
+ * 示例:
+ * 输入: 1->2->3->4->5->NULL, m = 2, n = 4
+ * 输出: 1->4->3->2->5->NULL
+ */
+
+
+public class ReverseLinkedList2 {
+    public static ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode mhead = head, nhead = new ListNode(0);
+        ListNode res = new ListNode(0);
+        res.next = head;
+        ListNode temp = res;
+        int i = 0;
+        while (temp.next != nhead) {
+            if (i == m - 1) mhead = temp;
+            if (i == n) {
+                //走到n时，把游标切换到mhead位置，接着循环
+                nhead = temp;
+                temp = mhead;
+            }
+
+            if (i >= n) {
+                //把temp.next插入到到n所在节点的位置后边
+                ListNode cur = nhead.next;
+                ListNode tempNext = temp.next;
+                nhead.next = temp.next;
+                temp.next = temp.next.next;
+                tempNext.next = cur;
+            }
+            i++;
+            if (i <= n) temp = temp.next;
+        }
+        return res.next;
+    }
+
+    public static ListNode reverseBetween1(ListNode head, int m, int n) {
+        ListNode res = new ListNode(0);
+        res.next = head;
+        ListNode mpre = res;
+        //找m前一个节点
+        for (int i = 0; i < m - 1; i++) {
+            mpre = mpre.next;
+        }
+        //最初为mpre后一个点
+        ListNode cur = mpre.next;
+        //最初为mpre后第二个点
+        ListNode curn = cur.next;
+        //最初为mpre后第三个点
+        ListNode curnn = curn.next;
+        for (int i = 0; i < n - m; i++) {
+            //反转后一个点和后两个点，后二连接后一
+            curnn = curn.next;
+            curn.next = cur;
+            //指针后移
+            cur = curn;
+            curn = curnn;
+        }
+        //最终curn落在第n个后面的点，cur落在第n个点
+        mpre.next.next = curn;
+        mpre.next = cur;
+        return res.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode line = Tool.stringToListNode("[1,2,3,4,5]");
+        ListNode res = reverseBetween1(line, 1, 4);
+
+    }
+
+}
