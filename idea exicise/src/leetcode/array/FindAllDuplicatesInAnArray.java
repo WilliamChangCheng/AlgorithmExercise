09fb78a69edcd39005a0b51794b9ec31c8d895bb
william <changcheng0609@gmail.com>
2019-05-14 17:17:35 +0800

    From God

---
+++ idea exicise/src/leetcode/array/FindAllDuplicatesInAnArray.java
@@ -0,0 +1,30 @@
+package leetcode.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+ * Find all the elements that appear twice in this array.
+ * Could you do it without extra space and in O(n) runtime?
+ *
+ * Example:
+ * Input:
+ * [4,3,2,7,8,2,3,1]
+ * Output:
+ * [2,3]
+ */
+public class FindAllDuplicatesInAnArray {
+    //利用数组输入的特点 1<=a(n)<=n,则可以直接利用原数组当hash表用。因为原数组都是正数，标为负数表示出现过，就可以找出所有出现过的数。
+    public List<Integer> findDuplicates1(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                res.add(index + 1);
+            }
+            nums[index] = -nums[index];
+        }
+        return res;
+    }
+}
